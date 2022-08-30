e2d5959328f36bebb0b135ef962b1a307525ab05
Facundo Bauza <fbauza2014@gmail.com>
2022-08-30 00:10:20 -0300

    Commit1

---
+++ DataAccessLayer/Implementations/DAL_Personas_SQL.cs
@@ -0,0 +1,98 @@
+﻿using DataAccessLayer.Interfaces;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Implementations
+{
+    public class DAL_Personas_SQL : IDAL_Personas
+    {
+       
+        private string sqlConnection = "Data Source=DESKTOP-O6DTQ45\\ROOT; Initial Catalog=Practico3; User ID=sa; Password=1234;";
+        public Persona AddPersona(Persona x)
+        {
+            using (var connection = new SqlConnection(sqlConnection))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO PERSONAS VALUES(@nombre, @documento)", connection);
+                {
+                    cmd.Parameters.Add(new SqlParameter("nombre", x.Nombre));
+                    cmd.Parameters.Add(new SqlParameter("documento", x.Documento));
+                    connection.Open();
+
+                    int result = cmd.ExecuteNonQuery();
+                }   
+            }
+
+            return x;
+        }
+
+        public Persona Get(string documento)
+        {
+            Persona result = null;
+
+            using (SqlConnection connection = new SqlConnection(sqlConnection))
+            {
+                string query = "SELECT Nombre, Documento FROM Personas WHERE Documento = @documento";
+                SqlCommand com = new SqlCommand(query, connection);
+                com.Parameters.AddWithValue("@documento", documento);
+
+                connection.Open();
+                SqlDataReader Reader = com.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    result = new Persona();
+
+                    result.Documento = Reader["Documento"].ToString();
+                    result.Nombre = Reader["Nombre"].ToString();
+
+                    Console.WriteLine(result.Nombre);
+
+                }
+                else
+                {
+                    Console.WriteLine("no llegamos");
+                }
+            }
+
+            return result;
+        }
+
+        public List<Persona> GetPersonas()
+        {
+
+            List<Persona> result = null;
+
+            using (var connection = new SqlConnection(sqlConnection))
+            {
+                result = new List<Persona>();
+                string query = "SELECT * FROM Persona";
+                SqlCommand com = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader Reader = com.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    if (result == null)
+                    {
+                        result = new List<Persona>();
+                    }
+
+                    Persona P = new Persona();
+
+                    P.Documento = Reader["documento"].ToString();
+                    P.Nombre = Reader["Nombre"].ToString();
+
+                    result.Add(P);
+                }
+
+            }
+            return result;
+        }
+    }
+}
---
+++ Practico3/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BusinessLayer.Implementations;
+using BusinessLayer.Interfaces;
+using DataAccessLayer.Implementations;
+using DataAccessLayer.Interfaces;
+using Shared;
+
+IDAL_Personas dalPersonas = new DAL_Personas_SQL();
+IBL_Personas blPersonas = new BL_Personas(dalPersonas);
+
+Console.WriteLine("Mi Primera APP...");
+
+do
+{
+    try
+    {
+        Console.WriteLine("##### Nueva Persona #####");
+
+
+        Persona persona = new Persona();
+        Console.WriteLine("Nombre: ");
+        persona.Nombre = Console.ReadLine();
+        Console.WriteLine("Documento: ");
+        persona.Documento = Console.ReadLine();
+
+        blPersonas.AddPersona(persona);
+
+        persona = blPersonas.Get(persona.Documento);
+
+        Console.WriteLine("Prueba del GET");
+        Console.WriteLine("Persona:");
+        Console.WriteLine(" -Nombre: " + persona.Nombre);
+        Console.WriteLine(" -Documento: " + persona.Documento);
+
+        Console.WriteLine();
+        Console.WriteLine("Prueba del GET ALL");
+        foreach (Persona x in blPersonas.GetPersonas())
+        {
+            Console.WriteLine("Persona:");
+            Console.WriteLine(" -Nombre: " + x.Nombre);
+            Console.WriteLine(" -Documento: " + x.Documento);
+            Console.WriteLine();
+        }
+
+
+    } catch (Exception ex) 
+    {
+        Console.WriteLine("ERROR: " + ex.Message);
+    }
+} while (!Console.ReadLine().Equals("exit"));
+
+
+
